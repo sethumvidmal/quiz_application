b28281a011c4372caa1e2dda1f1b94323c6459de
Sethum Vidmal <sethumthabrew@gmail.com>
2023-12-02 22:23:33 +0530

    Adding json file

--- lib/question_result.dart
+++ lib/question_result.dart
@@ -8,29 +8,28 @@ class QuestionResult extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
       child: Column(
-        // crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          // SizedBox(height: 20,),
-          Text(
-              'Question ${(value['questionIndex'] as int) + 1} : ${value['question'].toString()}',
-              style: const TextStyle(
-                  fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
-              textAlign: TextAlign.center),
-          Text('Your answer : ${((value['userAnswer'].toString()))}',
-              style: TextStyle(
-                  fontSize: 20,
-                  color: value['userAnswer'].toString() ==
-                          value['correct_answer'].toString()
-                      ? Colors.green
-                      : Colors.red),
-              textAlign: TextAlign.center),
-          Text('Correct answer : ${value['correct_answer'].toString()}',
-              style: const TextStyle(fontSize: 20, color: Colors.blue),
-              textAlign: TextAlign.center)
-        ],
-      ),
+      // crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        // SizedBox(height: 20,),
+        Text(
+            'Question ${(value['questionIndex'] as int) + 1} : ${value['question'].toString()}',
+            style: const TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+            textAlign: TextAlign.center),
+        Text('Your answer : ${((value['userAnswer'].toString()))}',
+            style: TextStyle(
+                fontSize: 20,
+                color: value['userAnswer'].toString() ==
+                        value['correct_answer'].toString()
+                    ? Colors.green
+                    : Colors.red),
+            textAlign: TextAlign.center),
+        Text('Correct answer : ${value['correct_answer'].toString()}',
+            style: const TextStyle(fontSize: 20, color: Colors.blue),
+            textAlign: TextAlign.center)
+      ],
+    ),
     );
   }
 }
